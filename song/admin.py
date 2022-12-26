39a1d0898701dd07d14bddfe401d0df0066703ff
aliyurekli32 <adamjesperblack@gmail.com>
2022-12-26 16:02:55 +0300

    Feat: admin.py fix

--- song/admin.py
+++ song/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Artist,Lyric,Song,Album
+
+
+# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Lyric)
+admin.site.register(Song)
+admin.site.register(Album)
 
 # Register your models here.
